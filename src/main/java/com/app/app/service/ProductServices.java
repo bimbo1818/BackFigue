cbef12cbf9bf5850b78949cdf06a218e5979ca13
Bimbo1818 <Bimbo18018@gmail.com>
2023-10-16 12:39:47 -0300

    backFigue

---
+++ src/main/java/com/app/app/service/ProductServices.java
@@ -0,0 +1,62 @@
+package com.app.app.service;
+import com.app.app.model.ProductoResponse;
+import com.app.app.model.Productos;
+import com.app.app.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServices {
+
+    private final ProductRepository ProductRepository;
+    public Productos createProduct(Productos product) {
+        return ProductRepository.save( product);
+    }
+
+    public List<ProductoResponse> getProducts() {
+        List<Productos> products = ProductRepository.findAll();
+
+        List<ProductoResponse> productsResponse = products.stream()
+                
+                .map(product -> {
+                    try {
+                        String relativePath = "static/"+product.getImage();
+
+                        Resource resource = new ClassPathResource(relativePath);
+                        InputStream inputStream = resource.getInputStream();
+                        byte[] content = inputStream.readAllBytes();
+
+                        //Resource imageResource = new ClassPathResource("static/"+ imageName);
+
+                        return new ProductoResponse(
+                                product.getIdProduct(),
+                                product.getNombre(),
+                                product.getDescripcion(),
+                                content,
+                                product.getCategoria()
+                        );
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toList());
+
+        return productsResponse;
+    }
+    public List<Productos> getProductsTest() {
+        return ProductRepository.findAll();
+    }
+
+
+}
+
+
